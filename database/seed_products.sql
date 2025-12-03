99e259da20234a99250b970470c81e3692d4a310
emb0dormo <aleksei.peu@student.mahidol.edu>
2025-12-04 03:23:48 +0700

    Add product sample data

---
+++ database/seed_products.sql
@@ -0,0 +1,14 @@
+-- Sample Products Data
+-- 10 coffee shop menu items
+
+INSERT INTO products(name, price) VALUES
+('Latte', 4.50),
+('Cappuccino', 4.00),
+('Espresso', 3.00),
+('Mocha', 5.00),
+('Americano', 3.50),
+('Flat White', 4.80),
+('Macchiato', 4.20),
+('Cold Brew', 4.70),
+('Iced Latte', 5.10),
+('Matcha Latte', 5.30);
